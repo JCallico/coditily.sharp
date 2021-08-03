3f17c30e7f85aa36147b70d53feb0ab6bb7cb25d
Javier Callico <jcallico@callicode.com>
2021-08-03 09:43:31 -0400

    Update DisappearingPairs.cs
    Replaced List with char array.

--- DisappearingPairs.cs
+++ DisappearingPairs.cs
@@ -5,71 +5,88 @@ https://app.codility.com/programmers/trainings/5/disappearing_pairs/
 */
 
 using System;
-using System.Collections.Generic;
 
 class Solution
 {
+    public string[] disappearingPairs = { "AA", "BB", "CC" };
+
     public String solution(String S)
     {
-        List<char> buffer = new List<char>(S);
-        List<char> temp = new List<char>();
+        char[] buffer = new char[S.Length];
+        char[] temp = new char[S.Length];
         var previousLength = 0;
+        var newLength = 0;
 
-        while (previousLength != buffer.Count)
+        for (var i = 0; i < S.Length; i++)
         {
-            previousLength = buffer.Count;
+            buffer[i] = S[i];
+        }
+
+        while (previousLength != buffer.Length)
+        {
+            previousLength = buffer.Length;
+            newLength = 0;
+
+            temp = new char[buffer.Length];
 
             // copying even characters
-            for (int i = 0; i < buffer.Count - 1; i += 2)
+            for (int i = 0; i < buffer.Length - 1; i += 2)
             {
                 if ((buffer[i] != 'A' || buffer[i + 1] != 'A') &&
                     (buffer[i] != 'B' || buffer[i + 1] != 'B') &&
                     (buffer[i] != 'C' || buffer[i + 1] != 'C'))
                 {
-                    temp.Add(buffer[i]);
-                    temp.Add(buffer[i + 1]);
+                    temp[newLength++] = buffer[i];
+                    temp[newLength++] = buffer[i + 1];
                 }
             }
 
             // copying last character
-            if (buffer.Count % 2 != 0)
+            if (buffer.Length % 2 != 0)
             {
-                temp.Add(buffer[buffer.Count - 1]);
+                temp[newLength++] = buffer[buffer.Length - 1];
             }
 
-            buffer = temp;
-            temp = new List<char>();
+            buffer = new char[newLength];
+            Array.Copy(temp, buffer, newLength);
 
-            if (buffer.Count < 2)
+            temp = new char[buffer.Length];
+
+            // resetting new length counter
+            newLength = 0;
+
+            if (buffer.Length < 2)
             {
                 continue;
             }
 
             // copying first character
-            temp.Add(buffer[0]);
+            temp[0] = buffer[0];
+
+            newLength++;
 
             // copying odd characters
-            for (int i = 1; i < buffer.Count - 1; i += 2)
+            for (int i = 1; i < buffer.Length - 1; i += 2)
             {
                 if ((buffer[i] != 'A' || buffer[i + 1] != 'A') &&
                     (buffer[i] != 'B' || buffer[i + 1] != 'B') &&
                     (buffer[i] != 'C' || buffer[i + 1] != 'C'))
                 {
-                    temp.Add(buffer[i]);
-                    temp.Add(buffer[i + 1]);
+                    temp[newLength++] = buffer[i];
+                    temp[newLength++] = buffer[i + 1];
                 }
             }
 
             // copying last character
-            if (buffer.Count % 2 == 0)
+            if (buffer.Length % 2 == 0)
             {
-                temp.Add(buffer[buffer.Count - 1]);
+                temp[newLength++] = buffer[buffer.Length - 1];
             }
 
-            buffer = temp;
-            temp = new List<char>();
+            buffer = new char[newLength];
+            Array.Copy(temp, buffer, newLength);
         }
 
-        return new string(buffer.ToArray());
+        return new string(buffer);
     }
 }
